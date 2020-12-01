f77b232ad3947c28ca9a6df312f8dfcacde0daba
Robin Laevaert <Robin.Laevaert@axxes.com>
2020-12-01 09:35:23 +0100

    Add project files.

---
+++ AOC2020/Days/Day_18.cs
@@ -0,0 +1,27 @@
+﻿using AOC2020.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2020.Days
+{
+    public class Day_18 : Day
+    {
+        public override void Part1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Part2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReadFile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AOC2020/Shared/Day.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+
+namespace AOC2020.Shared
+{
+    public abstract class Day
+    {
+        public string Title;
+        public int DayNumber;
+
+
+        public Day()
+        {
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{DayNumber}. {Title}");
+        }
+
+        public void HandleSelect()
+        {
+            Console.WriteLine("Do you want to solve Part 1 or 2?");
+
+            switch (Convert.ToDouble(Console.ReadLine()))
+            {
+                case 1:
+                    Part1();
+                    break;
+                case 2:
+                    Part2();
+                    break;
+                default:
+                    Console.WriteLine($"Not implemented");
+                    HandleSelect();
+                    break;
+            }
+        }
+
+        public void Deselect()
+        {
+            Console.WriteLine("Press Key to go back to main menu");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public string GetFilePath()
+        {
+            return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), $@"input{DayNumber}.txt");
+        }
+        public abstract void ReadFile();
+
+        public abstract void Part1();
+
+        public abstract void Part2();
+    }
+}
