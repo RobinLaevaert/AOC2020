c9669737cf3a6354ce3e419cd6eccdfc1dc0d651
Robin Laevaert <Robin.Laevaert@axxes.com>
2020-12-03 16:12:28 +0100

    Gotta love linq even if it's slower

--- Days/Day_03/Day_03.cs
+++ Days/Day_03/Day_03.cs
@@ -27,32 +27,18 @@ namespace Days
 
         public override void Part1()
         {
-            Console.WriteLine($"Trees encountered: {Get_trees_in_slope(3, 1)}");
+            Console.WriteLine($"Trees encountered: {field.Select(x => x[field.IndexOf(x)]).Count(x => x == '#')}");
         }
 
         public override void Part2()
         {
-            long trees_1_1 = Get_trees_in_slope(1, 1);
-            long trees_3_1 = Get_trees_in_slope(3, 1);
-            long trees_5_1 = Get_trees_in_slope(5, 1);
-            long trees_7_1 = Get_trees_in_slope(7, 1);
-            long trees_1_2 = Get_trees_in_slope(1, 2);
+            var trees_1_1 = field.Select(x => x[field.IndexOf(x)]).Count(x => x == '#');
+            var trees_3_1 = field.Select(x => x[field.IndexOf(x)*3]).Count(x => x == '#');
+            var trees_5_1 = field.Select(x => x[field.IndexOf(x)*5]).Count(x => x == '#');
+            var trees_7_1 = field.Select(x => x[field.IndexOf(x)*7]).Count(x => x == '#');
+            var trees_1_2 = field.Where(x => field.IndexOf(x)%2 == 0).Select(x => x[field.IndexOf(x)/2]).Count(x => x == '#');
 
             Console.WriteLine($"Product of trees encountered: {trees_1_1 * trees_3_1 * trees_5_1 * trees_7_1 * trees_1_2}");
-        }
-
-        public int Get_trees_in_slope(int vX, int vY)
-        {
-            var x = 0;
-            var y = 0;
-            var trees_count = 0;
-            while (y < field.Count)
-            {
-                if (field[y][x] == '#') trees_count++;
-                x += vX;
-                y += vY;
-            }
-            return trees_count;
         }
     }
 }
