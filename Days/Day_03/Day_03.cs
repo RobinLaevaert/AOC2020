9e99bf291df20d6d9877c4e4bde2d633c9a249a8
Robin Laevaert <Robin.Laevaert@axxes.com>
2020-12-03 16:34:07 +0100

    cleaner

--- Days/Day_03/Day_03.cs
+++ Days/Day_03/Day_03.cs
@@ -25,8 +25,16 @@ namespace Days
 
         public override void Part1() => Console.WriteLine($"Trees encountered: {Get_trees_in_slope(3, 1)}");
 
-        public override void Part2() => 
-            Console.WriteLine($"Product of trees encountered: {(long)Get_trees_in_slope(1, 1) * Get_trees_in_slope(3, 1) * Get_trees_in_slope(5, 1) * Get_trees_in_slope(7, 1) * Get_trees_in_slope(1, 2)}");
+        public override void Part2()
+        {
+            var trees_1_1 = Get_trees_in_slope(1, 1);
+            var trees_3_1 = Get_trees_in_slope(3, 1);
+            var trees_5_1 = Get_trees_in_slope(5, 1);
+            var trees_7_1 = Get_trees_in_slope(7, 1);
+            var trees_1_2 = Get_trees_in_slope(1, 2);
+
+            Console.WriteLine($"Product of trees encountered: {(long)trees_1_1 * trees_3_1 * trees_5_1 * trees_7_1 * trees_1_2}");
+        }
 
         public int Get_trees_in_slope(int vX, int vY) => field.Select((x, i) => x[(vX * i) / vY]).Where((x, i) => i % vY == 0).Count(x => x == '#');
     }
