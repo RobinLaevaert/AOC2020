a519df6b2be09454836299c7b30562362e04e48e
Robin Laevaert <Robin.Laevaert@axxes.com>
2020-12-10 13:31:52 +0100

    Cleanup

--- Days/Day_10/Day_10.cs
+++ Days/Day_10/Day_10.cs
@@ -1,4 +1,5 @@
 ﻿using AOC2020.Shared;
+using Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -56,34 +57,5 @@ namespace Days
             }
         }
     }
-    public class RouteSplit
-    {
-        public int Number { get; set; }
-        public IEnumerable<int> Routes { get; set; }
-    }
-   
-
-    public static class Extensions
-    {
-        public static IEnumerable<TResult> SelectWithPrevious<TSource, TResult>
-    (this IEnumerable<TSource> source,
-     Func<TSource, TSource, TResult> projection)
-        {
-            using (var iterator = source.GetEnumerator())
-            {
-                if (!iterator.MoveNext())
-                {
-                    yield break;
-                }
-                TSource previous = iterator.Current;
-                while (iterator.MoveNext())
-                {
-                    yield return projection(previous, iterator.Current);
-                    previous = iterator.Current;
-                }
-            }
-        }
-    }
-
 }
 
---
+++ Days/Day_10/RouteSplit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Days
+{
+    public class RouteSplit
+    {
+        public int Number { get; set; }
+        public IEnumerable<int> Routes { get; set; }
+    }
+
+}
+
